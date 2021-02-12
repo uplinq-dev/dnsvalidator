7d08185d3148a9cedf76a67959ae37be5b1c7f7c
1elf-me <maurice@preuss.io>
2021-02-12 19:27:41 +0100

    fixed wildcards for A, AAAA and CNAME

--- src/Validator/AAAAValidation.php
+++ src/Validator/AAAAValidation.php
@@ -13,10 +13,12 @@ class AAAAValidation implements Validator
      */
     public function validate(array $record): bool
     {
-        if (Helper::startsWith($record['name'], '*.'))
-            $record['name'] = substr($record['name'], 2);
+        $name = $record['name'];
 
-        if(!filter_var($record['name'], FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME))
+        if (Helper::startsWith($name, '*.'))
+            $name = substr($record['name'], 2);
+
+        if(!filter_var($name, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME))
             throw new ValidateException('No valid name given.');
 
         if(!filter_var($record['content'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6))
